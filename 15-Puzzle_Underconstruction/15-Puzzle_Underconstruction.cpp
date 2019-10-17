7ae3bd4e0e55aea914f14d6c11412a0f55db8839
doupro <doupros@live.com>
2019-10-17 15:25:05 +0100

    adding some mistakes

--- 15-Puzzle_Underconstruction/15-Puzzle_Underconstruction.cpp
+++ 15-Puzzle_Underconstruction/15-Puzzle_Underconstruction.cpp
@@ -5,10 +5,11 @@
 
 using namespace std;
 
+const int global_number = 4;
 
 struct Arrays
 {
-	int array[4][4];
+	int array[global_number][global_number];
 };
 int  num;
 
@@ -19,7 +20,7 @@ void createArray()
 
 void fillAllCase() {};
 
-void printMap(int arr[4][4] , int column,int row) 
+void printMap(int arr[global_number][global_number] , int column,int row)
 {
 	for (int i = 0; i < row; i++)
 	{
@@ -46,41 +47,80 @@ void readFile() {};
 
 int main()
 {
-	int arr[4][4] = { {1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,0} };
+	int arr[global_number][global_number] = { {1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,0} };
 	createArray();
 	return 0;
 }
 
-vector<Arrays> Cases;
+vector<Arrays> possiable_Cases;
 
 bool check(Arrays arrays) 
 {
 	bool checkRepeat;
-	
-		for (int i = 0; i < Cases.size; i++)
+		for (int possiable_cases_size = 0; possiable_cases_size < possiable_Cases.size(); possiable_cases_size++)
 		{
-			for (int j = 0; j <16; j++)
+			for (int first_num_in_array = 0; first_num_in_array < global_number; first_num_in_array++)
 			{
-				if (arrays.array[][]== Cases.)
+				for (int second_num_in_array = 0; second_num_in_array < global_number; second_num_in_array++)
 				{
-
+					if (arrays.array[first_num_in_array][second_num_in_array]!=possiable_Cases[possiable_cases_size].array[first_num_in_array][second_num_in_array] && arrays.array[global_number-1][global_number-1]==0)
+					{
+						return true;
+					}
 				}
 			}
 		}
-	
 }
 
-int movement(int arr[4][4]) {
-	Arrays* UpSelArray = new Arrays;
-	moveUp(UpSelArray->array, 4, 4);
-	if (check(*UpSelArray))
+
+void movement(int arr[4][4]) {
+	Arrays* Up_Seleted_Array = new Arrays;
+	moveUp(Up_Seleted_Array->array, global_number, global_number);
+	if (check(*Up_Seleted_Array))
 	{
-		Cases.push_back(*UpSelArray);
+		possiable_Cases.push_back(*Up_Seleted_Array);
 	} 
 	else
 	{
-		delete UpSelArray;
-		UpSelArray = NULL;
+		delete Up_Seleted_Array;
+		Up_Seleted_Array = NULL;
 	}
+
+	Arrays* Down_Seleted_Array = new Arrays;
+	moveDown(Down_Seleted_Array->array, global_number, global_number);
+		if (check(*Down_Seleted_Array))
+		{
+			possiable_Cases.push_back(*Down_Seleted_Array);
+		}
+		else
+		{
+			delete Down_Seleted_Array;
+			Down_Seleted_Array = NULL;
+		}
+
+		Arrays* Left_Seleted_Array = new Arrays;
+		moveDown(Left_Seleted_Array->array, global_number, global_number);
+		if (check(*Left_Seleted_Array))
+		{
+			possiable_Cases.push_back(*Left_Seleted_Array);
+		}
+		else
+		{
+			delete Left_Seleted_Array;
+			Left_Seleted_Array = NULL;
+		}
+
+		Arrays* Right_Seleted_Array = new Arrays;
+		moveDown(Right_Seleted_Array->array, global_number, global_number);
+		if (check(*Right_Seleted_Array))
+		{
+			possiable_Cases.push_back(*Right_Seleted_Array);
+		}
+		else
+		{
+			delete Right_Seleted_Array;
+			Right_Seleted_Array = NULL;
+		}
+
 
 }
